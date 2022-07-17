d9e953c604fa144a9881b8d0339d4fe9c1c403bf
yuanzi2 <zhyuey2@mail2.sysu.edu.cn>
2022-07-17 11:37:10 +0800

    Plot 2D model distribution for one chromosome

---
+++ R/plot2DDistributionOneChr.R
@@ -0,0 +1,16 @@
+plot2DDistributionOneChr=function(model, trainngData, name1, name2, chr.sel, saveFile){
+  lableList=list("Alpha score"="posterior mean of alpha", "NN score" ="posterior mean of PCC")
+  xStart=list("Alpha score"=0, "NN score"=(-1))
+  xEnd=list("Alpha score"=3, "NN score"=(1))
+  pdf(saveFile, width=8, height=3.7)
+  gsea.layout <- layout(matrix(1:2,nrow = 1, byrow = T))
+  x=seq(xStart[[name1]], xEnd[[name1]], by=0.01)
+  hist(trainngData$x[,1], probability=TRUE, breaks=30, xlab=sprintf(paste0(lableList[[name1]], " (%s)"), chr.sel), main="");
+  lines(x, dnorm(x, mean=model$parms.emission$mu[[1]][[1]], sd=sqrt(model$parms.emission$sigma[[1]][[1]])), type='l', col="red");
+  lines(x, dnorm(x, mean=model$parms.emission$mu[[2]][[1]], sd=sqrt(model$parms.emission$sigma[[2]][[1]])), type='l', col="green");
+  x=seq(xStart[[name2]], xEnd[[name2]], by=0.01)
+  hist(trainngData$x[,2], probability=TRUE, breaks=30, xlab=sprintf(paste0(lableList[[name2]], " (%s)"), chr.sel), main="");
+  lines(x, dnorm(x, mean=model$parms.emission$mu[[1]][[2]], sd=sqrt(model$parms.emission$sigma[[1]][[4]])), type='l', col="red");
+  lines(x, dnorm(x, mean=model$parms.emission$mu[[2]][[2]], sd=sqrt(model$parms.emission$sigma[[2]][[4]])), type='l', col="green");
+  dev.off()
+}
